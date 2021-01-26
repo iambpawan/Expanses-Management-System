f14df6834c87e0d1c85cdb9f8601abf1fbf9498f
Pawan <bpavan0123@gmail.com>
2021-01-26 21:30:45 +0530

    Create utill.js

---
+++ util/utill.js
@@ -0,0 +1,7 @@
+const path = require('path');
+
+exports.rootdir = path.dirname(process.mainModule.filename);
+exports.MAILGUN_API_KEY="-------------------------------------------";
+exports.DOMAIN = '------------------------------';
+exports.isauth=false;
+exports.userid='null';
