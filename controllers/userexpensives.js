3d4b030604a978e77deb41d9e7f895d1c978185f
Pawan <bpavan0123@gmail.com>
2021-01-27 11:35:40 +0530

    userprofile edit

--- controllers/userexpensives.js
+++ controllers/userexpensives.js
@@ -1,12 +1,12 @@
 const Expensive = require('../models/expensive');
-const auth =require('../util/utill');
-const User=require('../models/user');
+const auth = require('../util/utill');
+const User = require('../models/user');
 const Moment = require('moment');
 exports.getAddExpensive = (req, res, next) => {
   res.render('user/edit-expensive', {
     pageTitle: 'Add expensive',
     path: '/user/edit-expensive',
-    isauth:auth.isauth,
+    isauth: auth.isauth,
     editing: false
   });
 };
@@ -16,11 +16,11 @@ exports.getExpensives = (req, res, next) => {
     // .select('title price -_id')
     // .populate('userId', 'name')
     .then(expensives => {
-     // console.log(products);
+      // console.log(products);
       res.render('user/expensives', {
         prods: expensives,
         pageTitle: 'user expensives',
-        isauth:auth.isauth,
+        isauth: auth.isauth,
         path: '/user/expensives'
       });
     })
@@ -43,7 +43,7 @@ exports.postAddExpensive = (req, res, next) => {
     .save()
     .then(result => {
       // console.log(result);
-     // console.log('Created expensive');
+      // console.log('Created expensive');
       res.redirect('/user/expensives');
     })
     .catch(err => {
@@ -58,7 +58,7 @@ exports.getEditExpensive = (req, res, next) => {
   }
   const prodId = req.params.productId;
   // console.log('edit------------------------------------------------------');
-  
+
   Expensive.findById(prodId)
     .then(expensive => {
       if (!expensive) {
@@ -68,7 +68,7 @@ exports.getEditExpensive = (req, res, next) => {
         pageTitle: 'Edit expensive',
         path: '/user/edit-expensive',
         editing: editMode,
-        isauth:auth.isauth,
+        isauth: auth.isauth,
         expensive: expensive
       });
     })
@@ -113,7 +113,7 @@ exports.getIndex = (req, res, next) => {
       res.render('user/dashboard', {
         prods: expensives,
         pageTitle: 'dashboard',
-        isauth:auth.isauth,
+        isauth: auth.isauth,
         path: '/user/dashboard'
       });
     })
@@ -131,31 +131,61 @@ exports.getProduct = (req, res, next) => {
       res.render('user/expensive-detail', {
         expensive: expensive,
         pageTitle: expensive.Purpose,
-        isauth:auth.isauth,
+        isauth: auth.isauth,
         path: '/user/expensive-detail',
-        moment:Moment,
+        moment: Moment,
       });
     })
     .catch(err => console.log(err));
 };
-exports.getUserProfile=(req,res,next)=>{
+exports.getUserProfile = (req, res, next) => {
   res.render('user/userdetails', {
     pageTitle: 'Profile',
     path: '/user/userdetails',
-    isauth:auth.isauth,
-    user:req.user,
-    moment:Moment,
+    isauth: auth.isauth,
+    user: req.user,
+    moment: Moment,
   });
 }
 
-exports.getAddProfile=(req,res,next)=>{
+exports.getAddProfile = (req, res, next) => {
   res.render('user/userprofile-edit', {
     pageTitle: 'Add expensive',
     path: '/user/userprofile-edit',
-    isauth:auth.isauth,
+    isauth: auth.isauth,
   });
 }
-exports.postAddProfile=(req,res,next)=>{
+exports.postAddProfile = (req, res, next) => {
   console.log('now u can edit ');
   console.log(req.body);
+
+  User.findOneAndUpdate({ "email": req.body.email },
+    {
+      $set: {
+        gender: req.body.gender, firstname: req.body.firstname, lastname: req.body.lastname, dob: req.body.dob,
+        tel: req.body.tel, profession: req.body.prof
+      }
+    },
+    function (error, result) {
+      if (error) {
+        return res.json({
+          status: false,
+          message: 'error ',
+          error: error
+        });
+      }
+      else if (result) {
+        console.log(result);
+        return res.json({
+          status: true,
+          message: 'Success..',
+          result: result
+        });
+      }
+      return res.json({
+        status: false,
+        message: 'Data is not found',
+        error: error
+      });
+    });
 }
--- util/utill.js
+++ util/utill.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 exports.rootdir = path.dirname(process.mainModule.filename);
-exports.MAILGUN_API_KEY="-------------------------------------------";
-exports.DOMAIN = '------------------------------';
+exports.MAILGUN_API_KEY="------------create u r account and add here key-------------------------------";
+exports.DOMAIN = '----------------domain name too--------------';
 exports.isauth=false;
 exports.userid='null';
